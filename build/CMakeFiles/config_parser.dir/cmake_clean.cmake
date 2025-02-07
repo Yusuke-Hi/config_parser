0fb342cf93811d1142f026ff97fed0390173606f
yusuke <yuusuke29413@gmail.com>
2025-02-08 08:03:45 +0900

    make sample and build

---
+++ build/CMakeFiles/config_parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/config_parser.dir/src/main.cpp.o"
+  "CMakeFiles/config_parser.dir/src/main.cpp.o.d"
+  "config_parser"
+  "config_parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/config_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
