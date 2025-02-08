abeff503f386c3699f4762e589966122bf9c8055
yusuke <yuusuke29413@gmail.com>
2025-02-08 20:59:41 +0900

    library

---
+++ tests/config_parser_test.cpp
@@ -0,0 +1,23 @@
+#include "config_parser.h"
+#include <gtest/gtest.h>
+
+TEST(ConfigParserTest, ParseValidYAMO){
+    Config cfg = load_config("test_config.yaml");
+    EXPECT_EQ(cfg.host, "localhost");
+    EXPECT_EQ(cfg.port, 5432);
+}
+
+TEST(ConfigParserTest, MissingFile){
+    EXPECT_THROW(load_config("non_existent.yaml"), std::runtime_error);
+}
+
+TEST(ConfigParserTest, DefaultValues){
+    Config cfg = load_config("test_config_missing_keys.yaml");
+    EXPECT_EQ(cfg.host, "127.0.0.1");
+    EXPECT_EQ(cfg.port, 3306);
+}
+
+int main (int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
