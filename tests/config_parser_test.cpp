5268c354df49747b6f41eb0ec83719c2dc4e463e
yusuke <yuusuke29413@gmail.com>
2025-02-15 20:51:10 +0900

    fix path

--- tests/config_parser_test.cpp
+++ tests/config_parser_test.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(ConfigParserTest, ParseValidYAML){
-    Config cfg = load_config("/home/yusuke/oss_dev/config_parser/tests/test_config.yaml");
+    Config cfg = load_config("../tests/test_config.yaml");
     EXPECT_EQ(cfg.host, "localhost");
     EXPECT_EQ(cfg.port, 5432);
 }
@@ -12,7 +12,7 @@ TEST(ConfigParserTest, MissingFile){
 }
 
 TEST(ConfigParserTest, DefaultValues){
-    Config cfg = load_config("/home/yusuke/oss_dev/config_parser/tests/test_config_missing_keys.yaml");
+    Config cfg = load_config("../tests/test_config_missing_keys.yaml");
     EXPECT_EQ(cfg.host, "127.0.0.1");
     EXPECT_EQ(cfg.port, 3306);
 }
